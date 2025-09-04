a0f70ed7a3c07b6a613c6181c9ecc806ff3130f4
KauanJuniorDev <kauan.silva.28alunos@gmail.com>
2025-09-04 19:25:31 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,45 @@
-const CACHE_NAME = "meu-pwa-k-v-cache-v1.02";
+const CACHE_NAME = "pwa-cache-v1.5";
 const FILES_TO_CACHE = [
-   "index.html",
-   "manifest.json"
+  "index.html",
+  "manifest.json"    
 ];
-self.addEventListener("install", event => {
-    event.waitUntil(
-	caches.open(CACHE_NAME).then(cache => {
-	  return cache.addAll(FILES_TO_CACHE);
-	})
-    );
-    self.skipWaiting();
-    });
-self.addEventListener("activate", event => {
-    event.waitUntil(
-	caches.keys().then(keys => {
-	  return Promise.all(
-	    keys.filter( key => key !== CACHE_NAME) .map (key => caches.delete (key))
-	  );
-	})
-    );
-    self.clients.claim();
-});
-self.addEventListener("fetch", event => {
-    event.respondWith(
-	caches.match (event.request).then(response => {
-	  return response || fetch(event.request);
-        })
-    );
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
 });
 
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => key !== CACHE_NAME && caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request).catch(() =>
+        caches.match(event.request).then((resp) => resp || caches.match())
+      )
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request).then((resp) => {
+        return (
+          resp ||
+          fetch(event.request).then((response) => {
+            return caches.open(CACHE_NAME).then((cache) => {
+              cache.put(event.request, response.clone());
+              return response;
+            });
+          })
+        );
+      })
+    );
+  }
+});
